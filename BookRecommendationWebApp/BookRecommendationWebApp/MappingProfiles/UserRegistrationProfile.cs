f00f9198a77c090c06bc8628552978514735618f
Bartosz Starosta <bstarosta77@gmail.com>
2020-09-26 17:28:39 +0200

    Added basic version of security

---
+++ BookRecommendationWebApp/BookRecommendationWebApp/MappingProfiles/UserRegistrationProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookRecommendationWebApp.Models.Accounts;
+
+namespace BookRecommendationWebApp.MappingProfiles
+{
+    public class UserRegistrationProfile : Profile
+    {
+        public UserRegistrationProfile()
+        {
+            CreateMap<UserRegistrationModel, User>();
+        }
+    }
+}
---
+++ BookRecommendationWebApp/BookRecommendationWebApp/Models/Accounts/UserRegistrationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookRecommendationWebApp.Models.Accounts
+{
+    public class UserRegistrationModel
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
